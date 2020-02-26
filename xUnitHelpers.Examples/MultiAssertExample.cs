644299a51e4fbb3130601cc299dbb34f28139d44
Ankur Sheel <ankur.sheel@gmail.com>
2020-02-26 16:19:22 +1300

    add example for multi assert

---
+++ xUnitHelpers.Examples/MultiAssertExample.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace xUnitHelpers.Examples
+{
+    public class MultiAssertExample
+    {
+        [Fact]
+        public void MultiAssertTest()
+        {
+            var result = 2 + 2;
+            AssertHelper.AssertMultiple(() => Assert.IsType<double>(result),
+                                        () => Assert.Equal(5, result),
+                                        () => Assert.Equal(4, result));
+        }
+    }
+}
