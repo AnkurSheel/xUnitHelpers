e5213cb15c5007095aea550cd1daaeb062c469b3
Ankur Sheel <ankur.sheel@gmail.com>
2020-10-22 18:56:28 +1300

    add examples for VerifyLogging

---
+++ xUnitHelpers.Examples/Moq/TestClassForVerifyLogging.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace xUnitHelpers.Examples.Moq
+{
+    public class TestClassForVerifyLogging
+    {
+        private readonly ILogger<TestClassForVerifyLogging> _logger;
+
+        public TestClassForVerifyLogging(ILogger<TestClassForVerifyLogging> logger)
+        {
+            _logger = logger;
+        }
+
+        public void SingleLog()
+        {
+            _logger.LogError("Error Message");
+        }
+
+        public void SameLogMultipleTimes()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                _logger.LogError("Error Message");
+            }
+        }
+
+        public void MultipleLogsOfSameType()
+        {
+            _logger.LogError("Error Message one");
+            _logger.LogError("Error Message two");
+            _logger.LogError("Error Message three");
+        }
+
+        public void MultipleLogsOfDifferentTypes()
+        {
+            _logger.LogDebug("Debug Message");
+            _logger.LogWarning("Warning Message");
+            _logger.LogError("Error Message");
+        }
+    }
+}
---
+++ xUnitHelpers.Examples/Moq/VerifyLoggingExamples.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using xUnitHelpers.Moq;
+
+namespace xUnitHelpers.Examples.Moq
+{
+    public class VerifyLoggingExamples
+    {
+        private readonly Mock<ILogger<TestClassForVerifyLogging>> _loggerMock;
+        private readonly TestClassForVerifyLogging _sut;
+
+        public VerifyLoggingExamples()
+        {
+            _loggerMock = new Mock<ILogger<TestClassForVerifyLogging>>();
+            _sut = new TestClassForVerifyLogging(_loggerMock.Object);
+        }
+
+        [Fact]
+        public void VerifySingleLog()
+        {
+            _sut.SingleLog();
+
+            _loggerMock.VerifyLogging("Error Message");
+        }
+
+
+        [Fact]
+        public void VerifyMultipleLogsOfSameType()
+        {
+            _sut.MultipleLogsOfSameType();
+
+            _loggerMock.VerifyLogging("Error Message one")
+                       .VerifyLogging("Error Message two")
+                       .VerifyLogging("Error Message three");
+        }
+
+        [Fact]
+        public void VerifySameLogMultipleTimes()
+        {
+            _sut.SameLogMultipleTimes();
+
+            _loggerMock.VerifyLogging("Error Message", times: Times.Exactly(3));
+        }
+
+
+        [Fact]
+        public void VerifyMultipleLogsOfDifferentTypes()
+        {
+            _sut.MultipleLogsOfDifferentTypes();
+
+            _loggerMock.VerifyLogging("Error Message")
+                       .VerifyLogging("Warning Message", LogLevel.Warning)
+                       .VerifyLogging("Debug Message", LogLevel.Debug);
+        }
+    }
+}
