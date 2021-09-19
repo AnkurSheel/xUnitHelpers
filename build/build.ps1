3eca91dfc79a030648158448e6f6f6b2f1e2244f
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-20 00:32:29 +1200

    buildserver

--- build/build.ps1
+++ build/build.ps1
@@ -10,21 +10,22 @@ Write-Host "Build id variable is $env:BUILD_BUILDID"
 
 Write-Host "Restoring tooling for gitversion"
 
-
 dotnet tool restore
 
 Write-Host "Get version"
-$version = dotnet dotnet-gitversion /nofetch | Out-String | ConvertFrom-Json
+# $version = dotnet dotnet-gitversion /nofetch | Out-String | ConvertFrom-Json
+dotnet dotnet-gitversion /l console /output buildserver /nofetch
+# Write-HOST "version"
+# Write-Output $version
 
-Write-HOST "version"
-Write-Output $version
+# $nugetVersion = $version.NuGetVersionV2
 
-$nugetVersion = $version.NuGetVersionV2
+# Write-Host "Build Library"
+# Write-Host "dotnet build --configuration $configuration /p:Version=$nugetVersion $project"
+# dotnet build --configuration $configuration /p:Version=$nugetVersion $project
 
-Write-Host "Build Library"
-Write-Host "dotnet build --configuration $configuration /p:Version=$nugetVersion $project"
-dotnet build --configuration $configuration /p:Version=$nugetVersion $project
+# Write-Host "Build Nuget Package"
+# Write-Host "dotnet build --configuration $configuration /p:Version=$nugetVersion $project"
+# dotnet pack $project --configuration $configuration --include-symbols --no-build --output $outFolder /p:Version=$nugetVersion
 
-Write-Host "Build Nuget Package"
-Write-Host "dotnet build --configuration $configuration /p:Version=$nugetVersion $project"
-dotnet pack $project --configuration $configuration --include-symbols --no-build --output $outFolder /p:Version=$nugetVersion
+Get-ChildItem -Path Env:
