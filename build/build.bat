444e1faabaed4d73eb570c5a38f129c030d6e760
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-19 22:03:40 +1200

    change to batch file

---
+++ build/build.bat
@@ -0,0 +1,12 @@
+echo "outFolder - %1"
+echo "configuration - %2"
+echo "project - %3"
+echo "pwd - %cd%"
+
+dir
+
+echo "Restoring tooling for gitversion"
+dotnet tool install GitVersion.Tool
+
+echo "Running gitversion to determine version"
+dotnet dotnet-gitversion /l console /output buildserver /nofetch
