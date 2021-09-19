16beb4390d081ec794c394b84d45215227d97970
Ankur Sheel <ankur.sheel@gmail.com>
2021-09-19 21:35:21 +1200

    run git version

--- build/build.sh
+++ build/build.sh
@@ -4,4 +4,11 @@ echo "outFolder - $1"
 echo "configuration - $2"
 echo "pwd - $PWD"
 
+ls
+
+echo "Restoring tooling for gitversion"
 dotnet tool install GitVersion.Tool
+
+echo "Running gitversion to determine version"
+version = $(dotnet dotnet-gitversion /l console /output buildserver /nofetch)
+echo "version - $version"
