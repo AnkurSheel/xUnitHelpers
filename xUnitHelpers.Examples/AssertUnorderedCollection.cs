f4d3932b3fb1dd38afe0dca0684d3b09b79be77e
Ankur Sheel <ankur.sheel@gmail.com>
2022-03-16 12:09:21 +1300

    test: add some more examples for AssertUnorderedCollection

--- xUnitHelpers.Examples/AssertUnorderedCollection.cs
+++ xUnitHelpers.Examples/AssertUnorderedCollection.cs
@@ -6,20 +6,83 @@ namespace xUnitHelpers.Examples
     public class AssertUnorderedCollection
     {
         [Fact]
-        public void AssertUnorderedCollectionTest()
+        public void If_content_matches_test_passes()
         {
-            var expectedList = new List<int>()
+            var expectedList = new List<int>
             {
                 1,
                 2,
                 3
             };
 
-            var actualList = new List<int>()
+            var actualList = new List<int>
             {
                 2,
                 3,
                 1
+            };
+
+            AssertHelper.AssertUnorderedCollection(expectedList, actualList);
+        }
+
+        [Fact(Skip = "This is a failing test")]
+        public void If_content_does_not_match_test_fails()
+        {
+            var expectedList = new List<int>
+            {
+                1,
+                2,
+                4,
+            };
+
+            var actualList = new List<int>
+            {
+                2,
+                3,
+                1
+            };
+
+            AssertHelper.AssertUnorderedCollection(expectedList, actualList);
+        }
+
+        [Fact(Skip = "This is a failing test")]
+        public void If_expected_has_more_elements_test_fails()
+        {
+            var expectedList = new List<int>
+            {
+                1,
+                2,
+                3,
+                4
+            };
+
+            var actualList = new List<int>
+            {
+                2,
+                3,
+                1,
+            };
+
+            AssertHelper.AssertUnorderedCollection(expectedList, actualList);
+        }
+
+        [Fact(Skip = "This is a failing test")]
+        public void If_actual_has_more_elements_test_fails()
+        {
+            var expectedList = new List<int>
+            {
+                1,
+                2,
+                3,
+            };
+
+            var actualList = new List<int>
+            {
+                2,
+                3,
+                1,
+                4,
+                5
             };
 
             AssertHelper.AssertUnorderedCollection(expectedList, actualList);
